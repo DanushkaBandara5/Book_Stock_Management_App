5254fdda9adea844841241cf89658e117bf31fa0
danushkakasun5 <danushkakasun5@gmail.com>
2023-08-22 21:50:13 +0530

    book add and display completed

--- front_end/src/app/app.component.ts
+++ front_end/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Book} from "./dto/book";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'front_end';
+  bookList:Array<Book> =[];
+  constructor(private http:HttpClient) {
+    http.get<Array<Book>>('http://localhost:8080/api/v1/books').
+    subscribe(bookList=>this.bookList=bookList);
+  }
+
+  saveBook(isbn: HTMLInputElement, title: HTMLInputElement) {
+    let isbn1 =isbn.value;
+    let title1=title.value;
+    if(!isbn.value){
+      isbn.select();
+    }
+    if(!title.value){
+      title.select();
+    }
+    this.http.post('http://localhost:8080/api/v1/books',
+      new Book(isbn.value,title.value)).subscribe((data)=>{
+        this.bookList.push(new Book(isbn1,title1));
+    });
+    isbn.value="";
+    title.value="";
+    isbn.select();
+
+
+
+  }
+
+  clearText(isbn: HTMLInputElement, title: HTMLInputElement) {
+    isbn.value="";
+    title.value="";
+    isbn.select();
+
+  }
 }
